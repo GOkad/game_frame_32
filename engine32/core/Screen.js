4ad2b97238b4b7da189384c6f26f7aa4d288164d
George Okadarov <george.okadarov@gmail.com>
2023-02-25 03:20:31 +0200

    Testing canvas

--- engine32/core/Screen.js
+++ engine32/core/Screen.js
@@ -3,23 +3,35 @@
  * 
  * using `env.symbols` because it's one char
  * shorter than `this.symbols`
+ * 
+ * using `scrn` because it's shortar than `this.e`
+ * using `var` because it's shortar than `const`
  */
+var scrn = document.querySelector("canvas");
+var ctx = scrn.getContext("2d");
 
 class Screen
 {
     constructor()
     {
-        this.element = document.querySelector("canvas");
+        env.width = window.innerWidth;
+        env.height = window.innerHeight;
+        env.font = 20;
+
+        scrn.width = env.width;
+        scrn.height = env.height;
     }
 
     clear()
     {
+        ctx.fillStyle = "#000";
+        ctx.fillRect(0, 0, env.width, env.height);
     }
-
+    
     write(message)
     {
-        var ctx = this.element.getContext("2d");
-        ctx.font = "30px Arial";
+        ctx.fillStyle = "#fff";
+        ctx.font = env.font+"px Arial";
         ctx.fillText(message, 10, 50);
     }
 
